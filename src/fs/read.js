a186addbbdc728d103f1949171e21ae24b334b95
shoxa <sllx.ok@gmail.com>
2024-01-19 22:17:26 +0500

    feat: feat read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const filename = 'fileToRead.txt';
+    const targetPathFilename = path.join(__dirname, 'files', filename);
+
+    try {
+      await fs.access(targetPathFilename);
+    } catch {
+      throw new Error('FS operation failed');
+    }
+
+    console.log(await fs.readFile(targetPathFilename, 'utf-8'));
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await read();
