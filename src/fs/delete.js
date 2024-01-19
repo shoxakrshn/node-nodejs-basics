ea36c37b5713bb3e4702cf7a613ef54e1cf07c8b
shoxa <sllx.ok@gmail.com>
2024-01-19 22:04:07 +0500

    feat: feat delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const targetPathFilename = path.join(
+      __dirname,
+      'files',
+      'fileToRemove.txt'
+    );
+
+    try {
+      await fs.access(targetPathFilename);
+    } catch {
+      throw new Error('FS operation failed');
+    }
+
+    await fs.rm(targetPathFilename);
+    console.log('File has been deleted');
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await remove();
