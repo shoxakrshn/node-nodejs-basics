5825ee1dbbfc9e593d2525c282e6ce23bd0329a5
shoxa <sllx.ok@gmail.com>
2024-01-21 01:10:40 +0500

    feat: feat cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+import childProcess from 'node:child_process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const scriptPath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = childProcess.spawn('node', [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  child.on('error', (err) => {
+    console.error('Error:', err);
+  });
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 'child']);
