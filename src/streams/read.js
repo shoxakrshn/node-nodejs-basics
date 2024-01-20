795ab79f67fa29c0b0182a7a24d5516467dda6ad
shoxa <sllx.ok@gmail.com>
2024-01-20 16:36:51 +0500

    feat: feat read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const targetPathFile = path.join(__dirname, 'files', 'fileToRead.txt');
+  const rs = fs.createReadStream(targetPathFile);
+  rs.pipe(process.stdout);
 };
 
 await read();
