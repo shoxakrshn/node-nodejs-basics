55df6a5ccc22020ced159764a1989985d20b967e
shoxa <sllx.ok@gmail.com>
2024-01-20 17:18:48 +0500

    feat: feat calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+const crypto = await import('node:crypto');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function calculateHash() {
+  const targetPathFile = path.join(
+    __dirname,
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+
+  const rs = fs.createReadStream(targetPathFile);
+  const ws = process.stdout;
+  const hashStream = crypto.createHash('sha256');
+
+  rs.on('data', (chunk) => hashStream.update(chunk));
+  rs.on('end', () => {
+    const hash = hashStream.digest('hex');
+    ws.write(hash);
+  });
+}
 
 await calculateHash();
