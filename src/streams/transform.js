db2d736e064a5bb0b060518faa27be38b2f9c09b
shoxa <sllx.ok@gmail.com>
2024-01-20 16:37:09 +0500

    feat: feat transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const rs = process.stdin;
+  const ws = process.stdout;
+
+  const ts = new Transform({
+    transform(chunk, enc, cb) {
+      const chunkString = chunk.toString().trim();
+      const reversedChunk = chunkString.split('').reverse().join('');
+
+      this.push(reversedChunk + '\n');
+      cb();
+    },
+  });
+
+  pipeline(rs, ts, ws, (error) => console.log(error.message));
 };
 
 await transform();
